ae05662a3619960b23070e75b046172bf3096462
Michaelis-G <m.gregoriades@proton.me>
2025-12-28 11:40:01 +0100

    refactor

--- AoC/utils.py
+++
@@ -1,8 +0,0 @@
-from time import time
-
-def TrackTime(func):
-    def wrapper(*a, **k):
-        start = time()
-        func(*a, **k)
-        print("  >> in %f ms" % ((time() - start)*1000))
-    return wrapper
