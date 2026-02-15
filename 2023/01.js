e57b3b0d162bafb8e2cc630b24a752a28e7458f0
Michaelis <m.gregoriades@proton.me>
2026-02-15 14:46:32 +0100

    2023-01

--- 2023/01.js
+++ 2023/01.js
@@ -18,31 +18,44 @@ const parseInput = () => {
     return data;
 }
 
-const readCalibration = (calibration) => {
-	if (calibration.length === 0) return 0;
+const readCalibration = (calibration, nums) => {
+    if (calibration.length === 0) return 0;
 
-	let first, second;
-	for (let i = 0; i < calibration.length; i++) {
-		if (/^\d$/.test(calibration[i])) {
-			first = calibration[i];
-			break;
-		}
-	}
-	for (let i = calibration.length - 1; i >= 0; i--) {
-		if (/^\d$/.test(calibration[i])) {
-			second = calibration[i];
-			break;
-		}
-	}
-	return parseInt(first+second);
+    const a = [];
+    const b = [];
+    for (const num of nums) {
+        let i = calibration.indexOf(num.n);
+        if (i !== -1) {
+            a.push({i, v: num.v});
+        }
+        i = calibration.lastIndexOf(num.n);
+        if (i !== -1) {
+            b.push({i, v: num.v});
+        }
+    }
+    a.sort((a, b) => a.i - b.i);
+    b.sort((a, b) => b.i - a.i);
+
+    return parseInt(a[0].v + b[0].v);
 }
 
 const part1 = (data) => {
     console.time('Time');
     let res = 0;
 
+    const nums = [
+        {n: '1', v: '1'},
+        {n: '2', v: '2'},
+        {n: '3', v: '3'},
+        {n: '4', v: '4'},
+        {n: '5', v: '5'},
+        {n: '6', v: '6'},
+        {n: '7', v: '7'},
+        {n: '8', v: '8'},
+        {n: '9', v: '9'},
+    ];
     for (const calibration of data.split('\n')) {
-	    res += readCalibration(calibration);
+        res += readCalibration(calibration, nums);
     }
 
     console.log('Part 1:', res);
@@ -53,6 +66,30 @@ const part2 = (data) => {
     console.time('Time');
     let res = 0;
 
+    const nums = [
+        {n: '1', v: '1'},
+        {n: '2', v: '2'},
+        {n: '3', v: '3'},
+        {n: '4', v: '4'},
+        {n: '5', v: '5'},
+        {n: '6', v: '6'},
+        {n: '7', v: '7'},
+        {n: '8', v: '8'},
+        {n: '9', v: '9'},
+        {n: 'one', v: '1'},
+        {n: 'two', v: '2'},
+        {n: 'three', v: '3'},
+        {n: 'four', v: '4'},
+        {n: 'five', v: '5'},
+        {n: 'six', v: '6'},
+        {n: 'seven', v: '7'},
+        {n: 'eight', v: '8'},
+        {n: 'nine', v: '9'},
+    ];
+    for (const calibration of data.split('\n')) {
+        res += readCalibration(calibration, nums);
+    }
+
     console.log('Part 2:', res);
     console.timeEnd('Time');
 }
@@ -60,7 +97,7 @@ const part2 = (data) => {
 /**
  * Main
  */
-console.log( `Advent Of Code ${year}-${day}`);
+console.log(`Advent Of Code ${year}-${day}`);
 console.log('==========================');
 part1(parseInput());
 console.log('--------------------------');
