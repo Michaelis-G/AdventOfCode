a8a24c25830630c267baee7628f154558c7282e2
Michaelis-G <m.gregoriades@proton.me>
2026-03-04 17:33:09 +0100

    2023-08

--- 2023/08.js
+++ 2023/08.js
@@ -1,5 +1,5 @@
 /**
- * Share your thoughts
+ * Escape the wastelands
  */
 
 const year = '2023';
@@ -15,9 +15,48 @@ const parseInput = () => {
     return data;
 }
 
+const parsePattern = (data) => {
+    /** change L/R to array indexes */
+    return data.split('\n')[0].split('').map(d => (d === 'L') ? 0 : 1);
+}
+
+const parseMap = (data) => {
+    data = data.split('\n');
+    data.shift();
+    data.shift();
+    return data.map(m => {
+        m = m.replace('(', '').replace(')', '');
+        return {
+            node: m.split(' = ')[0],
+            dest: m.split(' = ')[1].split(', '),
+        }
+    });
+}
+
+const findNextNode = (node, map, direction) => {
+    const n = map.find(n => n.node === node);
+    const dest = n.dest[direction];
+    return dest;
+}
+
 const part1 = (data) => {
     console.time('Time');
     let res = 0;
+
+    const pattern = parsePattern(data);
+    const map = parseMap(data);
+
+    console.log(map);
+    node = 'AAA';
+    let n = 0;
+    while(node !== 'ZZZ') {
+        node = findNextNode(node, map, pattern[n]);
+        n += 1;
+        res += 1;
+        if (n % (pattern.length) === 0) {
+            n = 0;
+        }
+    }
 
     console.log('Part 1:', res);
     console.timeEnd('Time');
