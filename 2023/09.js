50154f8a5ff6f2aaf10eec8408bcdde57cca9b61
Michaelis-G <m.gregoriades@proton.me>
2026-03-05 08:03:54 +0100

    2023-09

---
+++ 2023/09.js
@@ -0,0 +1,41 @@
+/**
+ * Share your thoughts
+ */
+
+const year = '2023';
+const day = '09';
+const fs = require('fs');
+
+const parseInput = () => {
+    let filename = `./data/${day}`;
+    if (process.argv.length > 2) {
+        filename += '.test';
+    }
+    let data = fs.readFileSync(filename, 'utf8');
+    return data;
+}
+
+const part1 = (data) => {
+    console.time('Time');
+    let res = 0;
+
+    console.log('Part 1:', res);
+    console.timeEnd('Time');
+}
+
+const part2 = (data) => {
+    console.time('Time');
+    let res = 0;
+
+    console.log('Part 2:', res);
+    console.timeEnd('Time');
+}
+
+/**
+ * Main
+ */
+console.log( `Advent Of Code ${year}-${day}`);
+console.log('==========================');
+part1(parseInput());
+console.log('--------------------------');
+part2(parseInput());
