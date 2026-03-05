742761802f6c139b21f59a2a620cd0b3f0eb1db3
Michaelis-G <m.gregoriades@proton.me>
2026-03-05 09:02:35 +0100

    2023-09

--- 2023/09.js
+++ 2023/09.js
@@ -1,5 +1,5 @@
 /**
- * Share your thoughts
+ * The hand glider
  */
 
 const year = '2023';
@@ -15,10 +15,37 @@ const parseInput = () => {
     return data;
 }
 
+const parseSeries = (data) => {
+    return data.split('\n').map(l => l.split(' ').map(i => parseInt(i)));
+}
+
+const getNextRow = (row) => {
+    const nextRow = [];
+    for (let i = 1; i < row.length; i++) {
+        nextRow.push(row[i] - row[i - 1]);
+    }
+    return nextRow;
+}
+
 const part1 = (data) => {
     console.time('Time');
     let res = 0;
 
+    const series = parseSeries(data).map(s => [s]);
+    for (const serie of series) {
+        while(true) {
+            const nextSerie = getNextRow(serie[serie.length - 1]);
+            serie.push(nextSerie);
+            if (nextSerie.filter(e => e === 0).length === nextSerie.length) {
+                break;
+            }
+        }
+        let next = 0;
+        for (let i = serie.length - 1; i >=0; i--) {
+            next += serie[i][serie[i].length - 1];
+        }
+        res += next;
+    }
     console.log('Part 1:', res);
     console.timeEnd('Time');
 }
@@ -26,6 +53,22 @@ const part1 = (data) => {
 const part2 = (data) => {
     console.time('Time');
     let res = 0;
+
+    const series = parseSeries(data).map(s => [s]);
+    for (const serie of series) {
+        while(true) {
+            const nextSerie = getNextRow(serie[serie.length - 1]);
+            serie.push(nextSerie);
+            if (nextSerie.filter(e => e === 0).length === nextSerie.length) {
+                break;
+            }
+        }
+        let prev = 0;
+        for (let i = serie.length - 1; i >=0; i--) {
+            prev = serie[i][0] - prev;
+        }
+        res += prev;
+    }
 
     console.log('Part 2:', res);
     console.timeEnd('Time');
