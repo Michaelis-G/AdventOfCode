84665f20b422a0dccc68818c305a9fcedda88bf4
Michaelis-G <m.gregoriades@proton.me>
2026-03-14 11:40:39 +0100

    2022-01

--- 2022/01.rs
+++ 2022/01.rs
@@ -1,84 +1,60 @@
 /**
- *
- *
- *
+ * 2022 Day 01: Calories
  */
 
-// parsing script arguments 
-use std::env;
-// reading from file
-use std::fs::read_to_string;
+mod common;
 
+use std::env;
+use common::args::check_test_mode;
+use common::data::parse_file;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let data: Vec<String>;
+    println!("Advent of Code {year} - Day {day:02}", year = 2022, day = 1);
 
-    println!("Advent of Code {year} - {day:02}", year = 2022, day = 1);
-
-    if is_testmode(args) {
-        data = parse_file(String::from("./data/01.test"));
-    } else {
-        data = parse_file(String::from("./data/01"));
-    }
+    let data: Vec<String> = parse_file(
+        String::from("./data/01"),
+        check_test_mode(args),
+    );
 
     println!("Part 1: {}", part_1(data.clone()));
     println!("Part 2: {}", part_2(data.clone()));
 }
 
-fn is_testmode(args: Vec<String>) -> bool {
-    if args.len() > 1 && args[1] == "test" {
-        return true;
-    }
-    return false;
-}
-
-fn parse_file(filename: String) -> Vec<String> {
-    read_to_string(filename)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
-}
-
 fn part_1(data: Vec<String>) -> u32 {
-    let mut elves: Vec<u32> = [0].to_vec();
-    let mut i = 0;
-    for food in data {
-        if food.len() == 0 {
-            i += 1;
-            elves.push(0);
+    let mut best_total: u32 = 0;
+    let mut current_total: u32 = 0;
+    for calories in data.iter() {
+        if calories.trim().is_empty() {
+            if current_total > best_total {
+                best_total = current_total;
+            }
+            current_total = 0;
             continue;
         }
-        let calories: u32 = food.to_string().parse::<u32>().unwrap();
-        elves[i] += calories;
+        current_total += calories.to_string().parse::<u32>().unwrap();
     }
-    
-    elves.sort();
-    return elves[ elves.len() - 1 ];
+    return best_total; 
 }
 
 fn part_2(data: Vec<String>) -> u32 {
     let mut res = 0;
 
-    let mut elves: Vec<u32> = [0].to_vec();
-    let mut i = 0;
-    for food in data {
-        if food.len() == 0 {
-            i += 1;
-            elves.push(0);
+    let mut elves_calories: Vec<u32> = [0].to_vec();
+    let mut current_total: u32 = 0;
+    for calories in data.iter() {
+        if calories.trim().is_empty() {
+            elves_calories.push(current_total);
+            current_total = 0;
             continue;
         }
-        let calories: u32 = food.to_string().parse::<u32>().unwrap();
-        elves[i] += calories;
+        current_total += calories.to_string().parse::<u32>().unwrap();
     }
-    elves.sort();
-    let len = elves.len();
-    for calories in &mut elves[ len - 3 .. ] {
-        res += *calories;
-    } 
-
-    return res;
+    if current_total > 0 {
+        elves_calories.push(current_total);
+    }
+    elves_calories.sort_by(|a, b| b.cmp(a));
+    return elves_calories[ .. 3 ].iter().fold(0, |a, v| a + v);
 }
 
