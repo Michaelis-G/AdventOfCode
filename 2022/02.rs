c8a4fe685c4884178b0c570df994df3e6e9e3ed2
Michaelis-G <m.gregoriades@proton.me>
2026-03-17 13:55:52 +0100

    2022-02

--- 2022/02.rs
+++ 2022/02.rs
@@ -1,3 +1,4 @@
+
 /**
  * 2022 Day 02: Rock, Paper, Scissor
  */
@@ -13,14 +14,49 @@ fn main() {
     let day = 2;
 
     let args: Vec<String> = env::args().collect();
-    println!("Advent of Code {year} - Day {day:02}", year, day);
+    println!("Advent of Code {year} - Day {day:02}");
 
     let data: Vec<String> = parse_file(
-        String::from("./data/{day:02}", day),
+        String::from(format!("./data/{day:02}")),
         check_test_mode(args),
     );
 
+    // println!("{:?}", data);
     println!("Part 1: {}", part_1(data.clone()));
-    println!("Part 2: {}", part_2(data.clone()));
+    // println!("Part 2: {}", part_2(data.clone()));
 }
 
+fn part_1(data: Vec<String>) -> i32 {
+    let mut res = 0;
+    for moves in data.iter() {
+        let gestures: Vec<String> = moves.split(' ').map(String::from).collect();
+        let elf = &gestures[0];
+        let me = &gestures[1];
+        let score = compare_chifumi(
+            me.to_string(),
+            elf.to_string(),
+        );
+        res += score;
+    }
+    return res;
+}
+
+fn compare_chifumi(a: String, b: String) -> i32 {
+    let val_a = get_chifumi_value(a);
+    let val_b = get_chifumi_value(b);
+    let cmp: i32 = val_a - val_b;
+    let score = match cmp {
+        0 => 3,
+        -1 | -2 => 0,
+        1 | 2 => 6,
+        _ => 9,
+    };
+    return score + val_a;
+}
+
+fn get_chifumi_value(a: String) -> i32 {
+    let keys = vec!["A", "B", "C", "X", "Y", "Z"];
+    let values = vec![1, 2, 3, 1, 2, 3];
+    let index = keys.iter().position(|&m| m == a);
+    return values[index.unwrap()];
+}
